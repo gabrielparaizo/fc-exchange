77531ceed2582916cd88ceafe87d8aefc5b22055
Gabriel Paraizo <36803487+gabrielparaizo@users.noreply.github.com>
2025-02-20 00:04:14 -0300

    raw: add a small change to app module
    Since both our app and mongodb are in containers, I needed to change the
    connection string to work properly

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -5,7 +5,7 @@ import { AssetsModule } from './assets/assets.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:root@localhost:27017/nest?authSource=admin&directConnection=true'), AssetsModule],
+  imports: [MongooseModule.forRoot('mongodb://root:root@mongodb:27017/nest?authSource=admin&directConnection=true'), AssetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
